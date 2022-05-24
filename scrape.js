ce7321ebce9a260aa3fbd85873b94a2404d283fa
Vince <vperezjr@syr.edu>
2022-05-23 20:28:42 -0400

    scrape all pages of sdvx eamuse site, write to file

--- scrape.js
+++ scrape.js
@@ -1,46 +1,86 @@
 import * as cheerio from 'cheerio';
 import fetch from 'node-fetch';
+import { writeFile } from 'node:fs';
 
 const SDVX_SONGLIST_BASE_URL = 'https://p.eagate.573.jp/game/eacsdvx/vi/music/index.html';
 
 function getDifficulty(cheerio_element, api) {
-    return api(cheerio_element).attr()['class'].toUpperCase() + ' ' + api(cheerio_element).text();
+    let difficulty_name = api(cheerio_element).attr()['class'].toUpperCase()
+    let difficulty_rating = api(cheerio_element).text()
+    return [difficulty_name, difficulty_rating];
 }
- 
-fetch(SDVX_SONGLIST_BASE_URL)
-.then(res => res.arrayBuffer())
-.then(buffer => {
-    const decoder = new TextDecoder('shift_jis');
-    const data = decoder.decode(buffer)
-    const $ = cheerio.load(data)
-    $('div.music').each((i, music_div) => {
-        $(music_div).find('.inner').each((i, song_html) => {
-            $(song_html).find('.info').each((i, song_html_div) => {
-                var p_tag_cheerios = $(song_html_div).find('p')
-                const title = $(p_tag_cheerios.get(0)).text()
-                const artist = $(p_tag_cheerios.get(1)).text()
-                const diff_nov = getDifficulty(p_tag_cheerios.get(2), $)
-                const diff_adv = getDifficulty(p_tag_cheerios.get(3), $)
-                const diff_exh = getDifficulty(p_tag_cheerios.get(4), $)
-                var diff_special = null
-                var pack_name = null
-                if ($(p_tag_cheerios.get(5)).attr()['class'] != undefined) {
-                    diff_special = getDifficulty(p_tag_cheerios.get(5), $)
-                    pack_name = $(p_tag_cheerios.get(6)).text()
-                }
-                else {
-                    pack_name = $(p_tag_cheerios.get(5)).text()
-                }
-                const difficulties = []
-                difficulties.push(diff_nov)
-                difficulties.push(diff_adv)
-                difficulties.push(diff_exh)
-                if (diff_special != null) {
-                    difficulties.push(diff_special)
-                }
-                console.log(`Title: ${title}, Difficulties: ${difficulties.join(' | ')} Artist: ${artist}, Pack Name: ${pack_name}`)
+
+// [
+//     {
+//         title: 'title',
+//         artist: 'artist',
+//         difficulties: {
+//             'nov' : ..
+//         }
+//         pack_name: 'name'
+//     }
+// ]
+
+let pageNum = 1;
+var breakLoop = false;
+let songs = []
+while (!breakLoop) {
+    await fetch(`${SDVX_SONGLIST_BASE_URL}?page=${pageNum}`)
+    .then(res => res.arrayBuffer())
+    .then(buffer => {
+
+        const decoder = new TextDecoder('shift_jis');
+        const data = decoder.decode(buffer)
+        const $ = cheerio.load(data)
+
+        if ($('.music').length == 0) {
+            breakLoop = true
+            return false;
+        }
+
+        $('div.music').each((i, music_div) => {
+            $(music_div).find('.inner').each((i, song_html) => {
+                $(song_html).find('.info').each((i, song_html_div) => {
+                    var p_tag_cheerios = $(song_html_div).find('p')
+                    const title = $(p_tag_cheerios.get(0)).text()
+                    const artist = $(p_tag_cheerios.get(1)).text()
+                    const diff_nov = getDifficulty(p_tag_cheerios.get(2), $)
+                    const diff_adv = getDifficulty(p_tag_cheerios.get(3), $)
+                    const diff_exh = getDifficulty(p_tag_cheerios.get(4), $)
+
+                    var diff_special = null
+                    var pack_name = null
+
+                    const difficulties = {}
+                    difficulties[diff_nov[0]] = diff_nov[1]
+                    difficulties[diff_adv[0]] = diff_adv[1]
+                    difficulties[diff_exh[0]] = diff_exh[1]
+
+                    if ($(p_tag_cheerios.get(5)).attr()['class'] != undefined) {
+                        diff_special = getDifficulty(p_tag_cheerios.get(5), $)
+                        pack_name = $(p_tag_cheerios.get(6)).text()
+                        difficulties[diff_special[0]] = diff_special[1]
+                    }
+                    else {
+                        pack_name = $(p_tag_cheerios.get(5)).text()
+                    }
+
+                    
+                    
+                    songs.push({
+                        title: title,
+                        artist: artist,
+                        difficulties: difficulties,
+                        pack_name: pack_name
+                    })
+
+                })
             })
         })
     })
-}) 
-        
+    pageNum++
+}
+
+console.log(songs.length)
+let json_string = JSON.stringify(songs, null, 4)
+writeFile('./songs.json', json_string, (err) => {})
